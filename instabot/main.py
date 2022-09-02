e088316004983c66ec9359a5cc030689904b17e1
DenDen047 <sh.mn.nat@gmail.com>
2022-09-02 13:48:57 +0200

    success to run the sample of instagrapi

---
+++ instabot/main.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python
+import yaml
+from instagrapi import Client
+
+
+if __name__ == '__main__':
+    # load config
+    with open('config.yaml', mode='rb') as f:
+        config = yaml.safe_load(f)
+
+    cl = Client()
+    account_info = config['account']
+    cl.login(account_info['username'], account_info['password'])
+
+    user_id = cl.user_id_from_username("emily_ratajkowski_official")
+    medias = cl.user_medias(user_id, 20)
